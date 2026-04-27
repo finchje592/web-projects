98bf979270d21af8747a8e0363fdd4869371900a
finchje592 <Finchje592@northwested.org>
2026-04-27 13:19:01 -0400

    Add files via upload

--- starter.js
+++ starter.js
@@ -2,27 +2,27 @@
 // 24 April 2026
 // EVIDENCE: JS Functions
 
-function getTriangle() {
-const base = parseFloat(prompt("enter the base here"));
+function getBase() {
+   const base = parseFloat(prompt("enter the base here"));
+  
+   return base;
+    
 
-const length = parseFloat(prompt("enter the length here"));
-
-return {
-    base: base,
-    length: length
-}
 }
 
 function getHeight() {
     const height = parseFloat(prompt("enter the height here"));
-  return  {
-        height: height,
-    }
+ 
+    return height 
+    
 }
 
 function triangleArea(base, height) {
-const area = 0.5 * base * height;
-return `the triangle with a base of ${base} ft and a height of ${height} ft has an area of ${area} sq. ft.`;
+  const area = 0.5 * base * height;
+  return `the triangle with a base of ${base} ft and a height of ${height} ft has an area of ${area} sq. ft.`;
 }
 
 console.log(triangleArea(15,10));
+
+
+
